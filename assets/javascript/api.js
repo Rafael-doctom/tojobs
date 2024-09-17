ccfc9256b6092550bedb9f8d03b617faeeee8c1e
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2024-09-17 16:23:08 -0300

    feat: this is where things come to life

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -4,73 +4,68 @@ function search() {
   alert(`Pesquisando por: ${job}, Localização: ${location}`);
 }
 
-
-async function getVagas() {
+async function getIssuesToJobs() {
 
   try {
     fetch("https://api.github.com/repos/frontendbr/vagas/issues")
       .then(response => response.json())
       .then(
         data => {
-          renderizarVagas(data)
-          console.log(data)
+          renderJobs(data)
         }
       )
   } catch (error) {
     console.error(`Erro: ${error.message}`);
   };
 
-
 }
 
-const renderizarVagas = (data) => {
+const renderJobs = (data) => {
 
   data.forEach(element => {
-    console.log(element.url)
+
+    // const modality = (element.labels && element.labels.length > 0) ? element.labels[0].name : '';
+    // const user = element.user.html_url
+
+    if (element.state === 'open') {
+      element.state = 'aberto'
+    }
+
+    if (element.title === element.title) {
+      document.getElementById('cards').innerHTML += `
+      <div class="card" id="card">
+
+                    <span class="modalidade">
+                       ${element.state}
+                    </span>
+
+                    <div class="format">
+                        <img src="assets/images/programmer.png" alt="">
+                        <div class="describe">
+                            <h2 class="title" id="title">
+                                ${element.title}
+                            </h2>
+
+                            <div class="details">
+                                <span class="date-job">🕒 ${Intl.DateTimeFormat('pt-BR').format(new Date(element.created_at))}</span>
+                            </div>
+
+                            <div class="click">
+                                <a href="${element.html_url}" target="_blank" class="click-button">VER DETALHES DA VAGA</a>
+                            </div>
+
+                            <span class="htmlUser">Por <a href="${element.user.html_url}" target="_blank">${element.user.login}</a></span>
+
+
+                        </div>
+                    </div>
+                </div>
+                `
+    }
+
   });
 
 }
 
-// function renderizarVagas(data) {
-//   const vagasContainer = document.getElementById('vagas-container');
 
-//   // Limpa o conteúdo existente no container
-//   vagasContainer.innerHTML = '';
-
-//   data.forEach((vaga) => {
-//     const card = document.createElement('div');
-//     card.className = 'card';
-
-//     const button = document.createElement('button');
-//     const link = document.createElement('a');
-//     link.href = vaga.html_url;
-//     link.textContent = 'Mais detalhes...';
-//     button.appendChild(link);
-
-//     const describe = document.createElement('div');
-//     describe.className = 'describe';
-
-//     const title = document.createElement('h1');
-//     title.classList.add("title")
-//     title.textContent = vaga.title;
-
-
-//     const span = document.createElement('span');
-//     if (vaga.state == 'open') {
-//       span.textContent = 'ABERTO'
-//     } else {
-//       span.textContent = 'FECHADO'
-//     }
-
-
-//     describe.appendChild(span);
-//     describe.appendChild(title);
-//     describe.appendChild(button);
-
-//     card.appendChild(describe);
-
-//     vagasContainer.appendChild(card);
-//   });
-// }
-
-getVagas();
+getIssuesToJobs();
